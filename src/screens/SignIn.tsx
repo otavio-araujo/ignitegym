a9d7d9a7c6f2b142c242d364768ba213450a1982
Otávio Araújo <otavio_araujo@hotmail.com>
2024-10-09 22:19:38 -0300

    SigIn screen: pulled in react-native-svg-transformer and configured.

--- src/screens/SignIn.tsx
+++ src/screens/SignIn.tsx
@@ -1,5 +1,7 @@
-import { VStack, Image } from "@gluestack-ui/themed"
+import { VStack, Image, Center, Text } from "@gluestack-ui/themed"
+
 import BackgroundImg from "@assets/background.png"
+import Logo from "@assets/logo.svg"
 
 export function SignIn() {
   return (
@@ -12,6 +14,12 @@ export function SignIn() {
         defaultSource={BackgroundImg}
         position="absolute"
       />
+      <Center my="$24">
+        <Logo />
+        <Text color="$gray100" fontSize="$sm">
+          Treine sua mente e seu corpo.
+        </Text>
+      </Center>
     </VStack>
   )
 }
