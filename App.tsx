85e68a4d174e658401a4d08e46f292cd0592e91a
Otávio Araújo <otavio_araujo@hotmail.com>
2024-10-09 21:03:08 -0300

    Google fonts Roboto installed. Fixed typescript version to 5.3.3

--- App.tsx
+++ App.tsx
@@ -1,20 +1,16 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { Text, View } from "react-native"
+import {
+  useFonts,
+  Roboto_700Bold,
+  Roboto_400Regular,
+} from "@expo-google-fonts/roboto"
 
 export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
-}
+  const [fontsLoaded] = useFonts({ Roboto_700Bold, Roboto_400Regular })
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+  return (
+    <View style={{ flex: 1, alignItems: "center", justifyContent: "center" }}>
+      {fontsLoaded ? <Text>Home</Text> : <View></View>}
+    </View>
+  )
+}
