dcfccbbe1ad6255a760ee0c7085005489f27a4f5
Otávio Araújo <otavio_araujo@hotmail.com>
2024-10-09 23:15:10 -0300

    SigIn screen: Input component.

---
+++ src/components/Input.tsx
@@ -0,0 +1,22 @@
+import { ComponentProps } from "react"
+import { Input as GluestackInput, InputField } from "@gluestack-ui/themed"
+
+type Props = ComponentProps<typeof InputField>
+export function Input({ ...rest }: Props) {
+  return (
+    <GluestackInput
+      bg="$gray700"
+      h="$14"
+      px="$4"
+      borderWidth="$0"
+      borderRadius="$md"
+    >
+      <InputField
+        color="$white"
+        fontFamily="$body"
+        placeholderTextColor="$gray300"
+        {...rest}
+      />
+    </GluestackInput>
+  )
+}
--- src/screens/SignIn.tsx
+++ src/screens/SignIn.tsx
@@ -1,7 +1,8 @@
-import { VStack, Image, Center, Text } from "@gluestack-ui/themed"
+import { VStack, Image, Center, Text, Heading } from "@gluestack-ui/themed"
 
 import BackgroundImg from "@assets/background.png"
 import Logo from "@assets/logo.svg"
+import { Input } from "@components/Input"
 
 export function SignIn() {
   return (
@@ -14,12 +15,20 @@ export function SignIn() {
         defaultSource={BackgroundImg}
         position="absolute"
       />
+
       <Center my="$24">
         <Logo />
         <Text color="$gray100" fontSize="$sm">
           Treine sua mente e seu corpo.
         </Text>
       </Center>
+      <VStack flex={1} px="$10" pb="$16">
+        <Center gap="$2">
+          <Heading color="$gray100">Acesse sua conta</Heading>
+          <Input placeholder="E-mail" />
+          <Input placeholder="Senha" />
+        </Center>
+      </VStack>
     </VStack>
   )
 }
