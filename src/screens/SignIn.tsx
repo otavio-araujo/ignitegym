06a9a4f74552e24d876052e7a4c9eeae1fb3005a
Otávio Araújo <otavio_araujo@hotmail.com>
2024-10-10 00:09:29 -0300

    SigIn screen: Concluding the screen with ScrollView.

--- src/screens/SignIn.tsx
+++ src/screens/SignIn.tsx
@@ -1,4 +1,11 @@
-import { VStack, Image, Center, Text, Heading } from "@gluestack-ui/themed"
+import {
+  VStack,
+  Image,
+  Center,
+  Text,
+  Heading,
+  ScrollView,
+} from "@gluestack-ui/themed"
 
 import Logo from "@assets/logo.svg"
 import BackgroundImg from "@assets/background.png"
@@ -8,42 +15,47 @@ import { Button } from "@components/Button"
 
 export function SignIn() {
   return (
-    <VStack flex={1} bg="$gray700">
-      <Image
-        source={BackgroundImg}
-        alt="Pessoas treinando"
-        w="$full"
-        h={624}
-        defaultSource={BackgroundImg}
-        position="absolute"
-      />
+    <ScrollView
+      contentContainerStyle={{ flexGrow: 1 }}
+      showsVerticalScrollIndicator={false}
+    >
+      <VStack flex={1} bg="$gray700">
+        <Image
+          source={BackgroundImg}
+          alt="Pessoas treinando"
+          w="$full"
+          h={624}
+          defaultSource={BackgroundImg}
+          position="absolute"
+        />
 
-      <Center my="$24">
-        <Logo />
-        <Text color="$gray100" fontSize="$sm">
-          Treine sua mente e seu corpo.
-        </Text>
-      </Center>
-      <VStack flex={1} px="$10" pb="$16">
-        <Center gap="$2">
-          <Heading color="$gray100">Acesse sua conta</Heading>
-          <Input
-            placeholder="E-mail"
-            keyboardType="email-address"
-            autoCapitalize="none"
-          />
-          <Input placeholder="Senha" secureTextEntry />
-
-          <Button title="Acessar" />
-        </Center>
-
-        <Center flex={1} justifyContent="flex-end" mt="$4">
-          <Text color="$gray100" fontSize="$sm" mb="$3" fontFamily="$body">
-            Ainda não tem acesso?
+        <Center my="$24">
+          <Logo />
+          <Text color="$gray100" fontSize="$sm">
+            Treine sua mente e seu corpo.
           </Text>
-          <Button title="Criar conta" variant="outline" />
         </Center>
+        <VStack flex={1} px="$10" pb="$16">
+          <Center gap="$2">
+            <Heading color="$gray100">Acesse sua conta</Heading>
+            <Input
+              placeholder="E-mail"
+              keyboardType="email-address"
+              autoCapitalize="none"
+            />
+            <Input placeholder="Senha" secureTextEntry />
+
+            <Button title="Acessar" />
+          </Center>
+
+          <Center flex={1} justifyContent="flex-end" mt="$4">
+            <Text color="$gray100" fontSize="$sm" mb="$3" fontFamily="$body">
+              Ainda não tem acesso?
+            </Text>
+            <Button title="Criar conta" variant="outline" />
+          </Center>
+        </VStack>
       </VStack>
-    </VStack>
+    </ScrollView>
   )
 }
