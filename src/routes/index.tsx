8c008dcc56928bc3514442bbd35b4521c25e65f6
Otávio Araújo <otavio_araujo@hotmail.com>
2024-10-10 00:46:58 -0300

    Changing navigation theme.

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,11 +1,19 @@
-import { NavigationContainer } from "@react-navigation/native"
+import { Box } from "@gluestack-ui/themed"
+import { NavigationContainer, DefaultTheme } from "@react-navigation/native"
 
 import { AuthRoutes } from "./auth.routes"
 
+import { gluestackUIConfig } from "../../config/gluestack-ui.config"
+
 export function Routes() {
+  const theme = DefaultTheme
+  theme.colors.background = gluestackUIConfig.tokens.colors.gray700
+
   return (
-    <NavigationContainer>
-      <AuthRoutes />
-    </NavigationContainer>
+    <Box flex={1} bg="$gray700">
+      <NavigationContainer theme={theme}>
+        <AuthRoutes />
+      </NavigationContainer>
+    </Box>
   )
 }
--- src/screens/SignUp.tsx
+++ src/screens/SignUp.tsx
@@ -19,7 +19,7 @@ export function SignUp() {
       contentContainerStyle={{ flexGrow: 1 }}
       showsVerticalScrollIndicator={false}
     >
-      <VStack flex={1} bg="$gray700">
+      <VStack flex={1}>
         <Image
           source={BackgroundImg}
           alt="Pessoas treinando"
