ac9ad55c0e317b79c67bf90ee6dc874856884f22
Otávio Araújo <otavio_araujo@hotmail.com>
2024-10-10 22:04:01 -0300

    Bottom tabs applyed.

--- src/routes/app.routes.tsx
+++ src/routes/app.routes.tsx
@@ -11,9 +11,9 @@ export function AppRoutes() {
   return (
     <Navigator>
       <Screen name="home" component={Home} />
-      <Screen name="exercise" component={Exercise} />
       <Screen name="history" component={History} />
       <Screen name="profile" component={Profile} />
+      <Screen name="exercise" component={Exercise} />
     </Navigator>
   )
 }
--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -2,6 +2,7 @@ import { Box } from "@gluestack-ui/themed"
 import { NavigationContainer, DefaultTheme } from "@react-navigation/native"
 
 import { AuthRoutes } from "./auth.routes"
+import { AppRoutes } from "./app.routes"
 
 import { gluestackUIConfig } from "../../config/gluestack-ui.config"
 
@@ -12,7 +13,7 @@ export function Routes() {
   return (
     <Box flex={1} bg="$gray700">
       <NavigationContainer theme={theme}>
-        <AuthRoutes />
+        <AppRoutes />
       </NavigationContainer>
     </Box>
   )
