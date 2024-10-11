1d5b37a375397d926a3a674b38762155ffaa67e4
Otávio Araújo <otavio_araujo@hotmail.com>
2024-10-10 23:50:19 -0300

    Group useState.

--- src/screens/Home.tsx
+++ src/screens/Home.tsx
@@ -1,15 +1,32 @@
-import React from "react"
-import { VStack } from "@gluestack-ui/themed"
+import { useState } from "react"
+import { HStack, VStack } from "@gluestack-ui/themed"
 
 import { HomeHeader } from "@components/HomeHeader"
 import { Group } from "@components/Group"
 
 export function Home() {
+  const [groupSelected, setGroupSelected] = useState("costas")
   return (
     <VStack flex={1}>
       <HomeHeader />
 
-      <Group name="Costas" />
+      <HStack>
+        <Group
+          name="Costas"
+          isActive={groupSelected === "costas"}
+          onPress={() => setGroupSelected("costas")}
+        />
+        <Group
+          name="Biceps"
+          isActive={groupSelected === "biceps"}
+          onPress={() => setGroupSelected("biceps")}
+        />
+        <Group
+          name="Triceps"
+          isActive={groupSelected === "triceps"}
+          onPress={() => setGroupSelected("triceps")}
+        />
+      </HStack>
     </VStack>
   )
 }
