10e7e78e87e440577fb9835b0381156142226555
Otávio Araújo <otavio_araujo@hotmail.com>
2024-10-11 12:34:48 -0300

    Profile: name and email data.

--- src/components/Input.tsx
+++ src/components/Input.tsx
@@ -1,21 +1,25 @@
 import { ComponentProps } from "react"
 import { Input as GluestackInput, InputField } from "@gluestack-ui/themed"
 
-type Props = ComponentProps<typeof InputField>
-export function Input({ ...rest }: Props) {
+type Props = ComponentProps<typeof InputField> & {
+  isReadOnly?: boolean
+}
+export function Input({ isReadOnly = false, ...rest }: Props) {
   return (
     <GluestackInput
-      bg="$gray700"
       h="$14"
-      px="$4"
       borderWidth="$0"
       borderRadius="$md"
       $focus={{
         borderWidth: 1,
         borderColor: "$green500",
       }}
+      isReadOnly={isReadOnly}
+      opacity={isReadOnly ? 0.5 : 1}
     >
       <InputField
+        px="$4"
+        bg="$gray700"
         color="$white"
         fontFamily="$body"
         placeholderTextColor="$gray300"
--- src/screens/Profile.tsx
+++ src/screens/Profile.tsx
@@ -1,8 +1,9 @@
+import { ScrollView, TouchableOpacity } from "react-native"
 import { Center, Text, VStack } from "@gluestack-ui/themed"
 
 import { ScreenHeader } from "@components/ScreenHeader"
-import { ScrollView } from "react-native"
 import { UserPhoto } from "@components/UserPhoto"
+import { Input } from "@components/Input"
 
 export function Profile() {
   return (
@@ -16,6 +17,27 @@ export function Profile() {
             alt="Foto do usuário"
             size="xl"
           />
+
+          <TouchableOpacity>
+            <Text
+              mt="$2"
+              mb="$8"
+              fontSize="$md"
+              color="$green500"
+              fontFamily="$heading"
+            >
+              Alterar foto
+            </Text>
+          </TouchableOpacity>
+
+          <Center gap="$4">
+            <Input placeholder="Otávio Araújo" bg="$gray600" isReadOnly />
+            <Input
+              placeholder="otavio_araujo@hotmail.com"
+              bg="$gray600"
+              isReadOnly
+            />
+          </Center>
         </Center>
       </ScrollView>
     </VStack>
