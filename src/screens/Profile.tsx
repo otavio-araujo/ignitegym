8fc21bc03421ba047f4cb77ef943a4d354bdfa7c
Otávio Araújo <otavio_araujo@hotmail.com>
2024-10-11 12:22:30 -0300

    Profile: User Photo

--- src/screens/Profile.tsx
+++ src/screens/Profile.tsx
@@ -1,11 +1,23 @@
-import { Text, VStack } from "@gluestack-ui/themed"
+import { Center, Text, VStack } from "@gluestack-ui/themed"
 
 import { ScreenHeader } from "@components/ScreenHeader"
+import { ScrollView } from "react-native"
+import { UserPhoto } from "@components/UserPhoto"
 
 export function Profile() {
   return (
     <VStack flex={1}>
       <ScreenHeader title="Perfil" />
+
+      <ScrollView contentContainerStyle={{ paddingBottom: 36 }}>
+        <Center mt="$6" px="$10">
+          <UserPhoto
+            source={{ uri: "https://github.com/otavio-araujo.png" }}
+            alt="Foto do usuário"
+            size="xl"
+          />
+        </Center>
+      </ScrollView>
     </VStack>
   )
 }
