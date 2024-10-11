3a0df830842bc68c094dae9674cd2c84db7f3f4b
Otávio Araújo <otavio_araujo@hotmail.com>
2024-10-11 11:45:50 -0300

    HistoryCard

---
+++ src/components/HistoryCard.tsx
@@ -0,0 +1,33 @@
+import { HStack, Text, VStack, Heading } from "@gluestack-ui/themed"
+
+export function HistoryCard() {
+  return (
+    <HStack
+      w="$full"
+      px="$5"
+      py="$4"
+      mb="$3"
+      rounded="$md"
+      bg="$gray600"
+      alignItems="center"
+      justifyContent="space-between"
+    >
+      <VStack mr="$5">
+        <Heading
+          color="$white"
+          fontSize="$md"
+          fontFamily="$heading"
+          textTransform="capitalize"
+        >
+          Costas
+        </Heading>
+        <Text color="$gray100" fontSize="$lg" numberOfLines={1}>
+          Puxada frontal
+        </Text>
+      </VStack>
+      <Text color="$gray300" fontSize="$md">
+        8:52
+      </Text>
+    </HStack>
+  )
+}
--- src/screens/History.tsx
+++ src/screens/History.tsx
@@ -2,10 +2,13 @@ import React from "react"
 
 import { VStack, Text } from "@gluestack-ui/themed"
 import { ScreenHeader } from "@components/ScreenHeader"
+import { HistoryCard } from "@components/HistoryCard"
 export function History() {
   return (
     <VStack flex={1}>
       <ScreenHeader title="Histórico" />
+
+      <HistoryCard />
     </VStack>
   )
 }
