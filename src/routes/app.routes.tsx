dc5db2ede7d1c78dc8e390f30bc7a5227a9b98a9
Otávio Araújo <otavio_araujo@hotmail.com>
2024-10-10 23:00:04 -0300

    Active and inactive bottom tab icons styled.

--- src/routes/app.routes.tsx
+++ src/routes/app.routes.tsx
@@ -28,7 +28,14 @@ export function AppRoutes() {
   const { tokens } = gluestackUIConfig
   const iconSize = tokens.space["6"]
   return (
-    <Navigator screenOptions={{ headerShown: false, tabBarShowLabel: false }}>
+    <Navigator
+      screenOptions={{
+        headerShown: false,
+        tabBarShowLabel: false,
+        tabBarActiveTintColor: tokens.colors.green500,
+        tabBarInactiveTintColor: tokens.colors.gray200,
+      }}
+    >
       <Screen
         name="home"
         component={Home}
