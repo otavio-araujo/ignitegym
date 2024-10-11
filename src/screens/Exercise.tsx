cdbd31901d2dae47e48adb4635434ac17c586b32
Otávio Araújo <otavio_araujo@hotmail.com>
2024-10-11 13:54:42 -0300

    ExerciseScreen done.

--- src/screens/Exercise.tsx
+++ src/screens/Exercise.tsx
@@ -1,18 +1,24 @@
-import { TouchableOpacity } from "react-native"
+import { ScrollView, TouchableOpacity } from "react-native"
 import { useNavigation } from "@react-navigation/native"
 import {
+  Box,
   Center,
   Heading,
   HStack,
   Icon,
+  Image,
   Text,
   VStack,
 } from "@gluestack-ui/themed"
 
 import { ArrowLeft } from "lucide-react-native"
+
 import BodySvg from "@assets/body.svg"
+import SeriesSvg from "@assets/series.svg"
+import RepetitionsSvg from "@assets/repetitions.svg"
 
 import { AppNavigatorRoutesProps } from "@routes/app.routes"
+import { Button } from "@components/Button"
 export function Exercise() {
   const navigation = useNavigation<AppNavigatorRoutesProps>()
 
@@ -48,6 +54,49 @@ export function Exercise() {
           </HStack>
         </HStack>
       </VStack>
+
+      <ScrollView
+        showsVerticalScrollIndicator={false}
+        contentContainerStyle={{ paddingBottom: 32 }}
+      >
+        <VStack p="$8">
+          <Image
+            source={{
+              uri: "https://static.wixstatic.com/media/2edbed_60c206e178ad4eb3801f4f47fc6523df~mv2.webp/v1/fill/w_350,h_375,al_c,q_80,enc_auto/2edbed_60c206e178ad4eb3801f4f47fc6523df~mv2.webp",
+            }}
+            resizeMode="cover"
+            alt="Exercício"
+            rounded="$lg"
+            mb="$3"
+            w="$full"
+            h="$80"
+          />
+
+          <Box bg="$gray600" rounded="$md" pb="$4" px="$4">
+            <HStack
+              alignItems="center"
+              justifyContent="space-around"
+              mb="$6"
+              mt="$5"
+            >
+              <HStack>
+                <SeriesSvg />
+                <Text color="$gray200" ml="$2">
+                  3 séries
+                </Text>
+              </HStack>
+              <HStack>
+                <RepetitionsSvg />
+                <Text color="$gray200" ml="$2">
+                  12 repetições
+                </Text>
+              </HStack>
+            </HStack>
+
+            <Button title="Marcar como realizado" />
+          </Box>
+        </VStack>
+      </ScrollView>
     </VStack>
   )
 }
