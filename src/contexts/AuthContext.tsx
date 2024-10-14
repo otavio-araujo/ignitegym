e55847f94720d6887393e3dbe5a80923e307da38
Otávio Araújo <otavio_araujo@hotmail.com>
2024-10-13 23:21:53 -0300

    Use token inside requests.

--- src/contexts/AuthContext.tsx
+++ src/contexts/AuthContext.tsx
@@ -6,6 +6,7 @@ import {
   storageUserRemove,
   storageUserSave,
 } from "@storage/storageUser"
+import { storageAuthTokenSave } from "@storage/storageAuthToken"
 
 export type AuthContextDataProps = {
   user: UserDTO
@@ -26,13 +27,29 @@ export function AuthContextProvider({ children }: AuthContextProviderProps) {
   const [user, setUser] = useState<UserDTO>({} as UserDTO)
   const [isLoadingUserStorageData, setIsLoadingUserStorageData] = useState(true)
 
+  async function storageUserAndToken(userData: UserDTO, token: string) {
+    try {
+      setIsLoadingUserStorageData(true)
+
+      api.defaults.headers.common["Authorization"] = `Bearer ${token}`
+
+      await storageUserSave(userData)
+      await storageAuthTokenSave(token)
+
+      setUser(userData)
+    } catch (error) {
+      throw error
+    } finally {
+      setIsLoadingUserStorageData(false)
+    }
+  }
+
   async function signIn(email: string, password: string) {
     try {
       const { data } = await api.post("/sessions", { email, password })
 
       if (data.user && data.token) {
-        setUser(data.user)
-        storageUserSave(data.user)
+        storageUserAndToken(data.user, data.token)
       }
     } catch (error) {
       throw error
