6f813eab1abbfff6fd1b9946310e595ec7e8a160
Otávio Araújo <otavio_araujo@hotmail.com>
2024-10-14 19:04:12 -0300

    Loading History.

--- src/screens/History.tsx
+++ src/screens/History.tsx
@@ -1,11 +1,21 @@
-import React, { useState } from "react"
 import { SectionList } from "react-native"
+import React, { useCallback, useState } from "react"
+import { useFocusEffect } from "@react-navigation/native"
 
-import { VStack, Heading, Text } from "@gluestack-ui/themed"
+import { VStack, Heading, Text, useToast } from "@gluestack-ui/themed"
+
+import { api } from "@services/api"
+import { AppError } from "@utils/AppError"
+
 import { HistoryCard } from "@components/HistoryCard"
 import { ScreenHeader } from "@components/ScreenHeader"
+import { ToastMessage } from "@components/ToastMessage"
 
 export function History() {
+  const toast = useToast()
+  const [isLoading, setIsLoading] = useState(true)
+  const [history, setHistory] = useState([])
+
   const [exercises, setExercises] = useState([
     {
       title: "10.10.24",
@@ -16,6 +26,40 @@ export function History() {
       data: ["Levantamento terra", "Agachamento", "Elevação lateral"],
     },
   ])
+
+  async function fetchHistory() {
+    try {
+      setIsLoading(true)
+      const { data } = await api.get("/history")
+
+      console.log(data)
+    } catch (error) {
+      const isAppErro = error instanceof AppError
+      const title = isAppErro
+        ? error.message
+        : "Não foi possível carregar o histórico."
+
+      toast.show({
+        placement: "top",
+        render: ({ id }) => (
+          <ToastMessage
+            id={id}
+            title={title}
+            action="error"
+            onClose={() => toast.close(id)}
+          />
+        ),
+      })
+    } finally {
+      setIsLoading(false)
+    }
+  }
+
+  useFocusEffect(
+    useCallback(() => {
+      fetchHistory()
+    }, [])
+  )
   return (
     <VStack flex={1}>
       <ScreenHeader title="Histórico de Exercícios" />
