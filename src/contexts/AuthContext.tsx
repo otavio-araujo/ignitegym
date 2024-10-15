d138eb3ab30bca76af70fd9a97f4f073523795de
Otávio Araújo <otavio_araujo@hotmail.com>
2024-10-15 16:01:59 -0300

    Storing refresh token

--- src/contexts/AuthContext.tsx
+++ src/contexts/AuthContext.tsx
@@ -11,6 +11,7 @@ import {
   storageAuthTokenRemove,
   storageAuthTokenSave,
 } from "@storage/storageAuthToken"
+import { ref } from "yup"
 
 export type AuthContextDataProps = {
   user: UserDTO
@@ -37,12 +38,16 @@ export function AuthContextProvider({ children }: AuthContextProviderProps) {
     setUser(userData)
   }
 
-  async function storageUserAndTokenSave(userData: UserDTO, token: string) {
+  async function storageUserAndTokenSave(
+    userData: UserDTO,
+    token: string,
+    refresh_token: string
+  ) {
     try {
       setIsLoadingUserStorageData(true)
 
       await storageUserSave(userData)
-      await storageAuthTokenSave(token)
+      await storageAuthTokenSave({ token, refresh_token })
     } catch (error) {
       throw error
     } finally {
@@ -54,8 +59,8 @@ export function AuthContextProvider({ children }: AuthContextProviderProps) {
     try {
       const { data } = await api.post("/sessions", { email, password })
 
-      if (data.user && data.token) {
-        await storageUserAndTokenSave(data.user, data.token)
+      if (data.user && data.token && data.refresh_token) {
+        await storageUserAndTokenSave(data.user, data.token, data.refresh_token)
 
         userAndTokenUpdate(data.user, data.token)
       }
@@ -93,7 +98,7 @@ export function AuthContextProvider({ children }: AuthContextProviderProps) {
       setIsLoadingUserStorageData(true)
 
       const currentLoggedUser = await storageUserGet()
-      const token = await storageAuthTokenGet()
+      const { token } = await storageAuthTokenGet()
 
       if (currentLoggedUser && token) {
         userAndTokenUpdate(currentLoggedUser, token)
--- src/storage/storageAuthToken.ts
+++ src/storage/storageAuthToken.ts
@@ -2,13 +2,28 @@ import AsyncStorage from "@react-native-async-storage/async-storage"
 
 import { AUTH_TOKEN_STORAGE } from "@storage/storageConfig"
 
-export async function storageAuthTokenSave(token: string) {
-  await AsyncStorage.setItem(AUTH_TOKEN_STORAGE, token)
+type storageAuthTokenProps = {
+  token: string
+  refresh_token: string
+}
+
+export async function storageAuthTokenSave({
+  token,
+  refresh_token,
+}: storageAuthTokenProps) {
+  await AsyncStorage.setItem(
+    AUTH_TOKEN_STORAGE,
+    JSON.stringify({ token, refresh_token })
+  )
 }
 
 export async function storageAuthTokenGet() {
-  const token = await AsyncStorage.getItem(AUTH_TOKEN_STORAGE)
-  return token
+  const response = await AsyncStorage.getItem(AUTH_TOKEN_STORAGE)
+
+  const { token, refresh_token }: storageAuthTokenProps = response
+    ? JSON.parse(response)
+    : {}
+  return { token, refresh_token }
 }
 
 export async function storageAuthTokenRemove() {
