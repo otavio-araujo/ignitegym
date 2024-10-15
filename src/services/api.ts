d42a21c594dd2ba6e90a2c759bc03a6506832278
Otávio Araújo <otavio_araujo@hotmail.com>
2024-10-15 16:42:40 -0300

    Refresh token done.

--- src/services/api.ts
+++ src/services/api.ts
@@ -65,10 +65,31 @@ api.registerInterceptTokenManager = (signOut) => {
               const { data } = await api.post("/sessions/refresh-token", {
                 refresh_token,
               })
+
               await storageAuthTokenSave({
                 token: data.token,
                 refresh_token: data.refresh_token,
               })
+
+              if (originalRequestConfig.data) {
+                originalRequestConfig.data = JSON.parse(
+                  originalRequestConfig.data
+                )
+              }
+
+              originalRequestConfig.headers = {
+                Authorization: `Bearer ${data.token}`,
+              }
+
+              api.defaults.headers.common[
+                "Authorization"
+              ] = `Bearer ${data.token}`
+
+              failedQueue.forEach((request) => {
+                request.onSuccess(data.token)
+              })
+
+              resolve(api(originalRequestConfig))
             } catch (error) {
               failedQueue.forEach((request) => {
                 request.onFailure(error as AxiosError)
