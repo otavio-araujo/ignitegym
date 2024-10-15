8605ea7785d54fadd6d9620bb78548d0e741759b
Otávio Araújo <otavio_araujo@hotmail.com>
2024-10-14 23:19:59 -0300

    Conditional password validation

--- src/screens/Profile.tsx
+++ src/screens/Profile.tsx
@@ -35,7 +35,12 @@ const profileSchema = yup.object({
     .string()
     .nullable()
     .transform((value) => (!!value ? value : null))
-    .oneOf([yup.ref("password"), null], "A confirmação de senha não confere."),
+    .oneOf([yup.ref("password"), null], "A confirmação de senha não confere.")
+    .when("password", {
+      is: (Field: any) => Field,
+      then: (schema) =>
+        schema.nullable().required("Informe a confirmação de senha"),
+    }),
 })
 
 export function Profile() {
