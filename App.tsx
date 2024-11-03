21c3caf6a1db1513cc8d981efa7d0f1df6023365
Otávio Araújo <otavio_araujo@hotmail.com>
2024-11-02 22:12:37 -0300

    onesignal-installed-and-configured

--- App.tsx
+++ App.tsx
@@ -8,11 +8,16 @@ import { StatusBar } from "react-native"
 import { config } from "./config/gluestack-ui.config"
 import { GluestackUIProvider } from "@gluestack-ui/themed"
 
-import { AuthContext, AuthContextProvider } from "@contexts/AuthContext"
+import { AuthContextProvider } from "@contexts/AuthContext"
 
 import { Routes } from "@routes/index"
 
 import { Loading } from "@components/Loading"
+
+import { OneSignal } from "react-native-onesignal"
+
+OneSignal.initialize("abdf8fe7-23a0-4eea-96a0-3653a01a8c23")
+OneSignal.Notifications.requestPermission(true)
 
 export default function App() {
   const [fontsLoaded] = useFonts({ Roboto_700Bold, Roboto_400Regular })
